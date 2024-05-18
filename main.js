2343ea9b2880d1f8f119b00a1411a3261c43454c
Alessio-Salerno <156314493+Alessio-Salerno@users.noreply.github.com>
2024-05-18 15:21:45 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+let num1 = 50;
+
+let num2 = 6;
+
+// STAMPA IN CONSOLE DELLA PRIMA VARIABILE
+
+console.log('Il valore della prima variabile è :' + num1 );
+
+// STAMPA IN CONSOLE DELLA SECONDA VARABILE
+
+console.log("Il valore della seconda variabile e' :  " + num2 ) ; 
+
+// STRINGA
+
+let stringa = 'Ciao a tutti';
+console.log("Il valore derlla stringa e' " + stringa);
+
+// CAMBIO VALORE STRINGA E STAMPA
+
+stringa = 'Salve a tutti sono Alessio' ;
+console.log("il valore della stringa e' cambiato in " + stringa);
+
+// CONSTANTE
+
+const PIGRECO = 3.14 ;
+console.log(PIGRECO);
+
+PIGRECO = 2.20;
+console.log(PIGRECO);
